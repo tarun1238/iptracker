f4b208f0d3cd390f8b2af9abefce9d4e4188c3cc
tarun1238 <kumartarun1238@gmail.com>
2021-01-09 20:59:28 +0530

    code3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, {useState} from 'react'
 import ip from 'ip'
-import prepareUrls from 'local-ip-url/prepareUrls'
 function App(){
 
   const details = useState(true);
   const handleClick=()=>{
     document.write(ip.address())
-    console.log(prepareUrls({
-      protocol: 'http',
-      host: '0.0.0.0',
-      port: 3000
-    }))
+    var findIP = new Promise(r=>{var w=window,a=new (w.RTCPeerConnection||w.mozRTCPeerConnection||w.webkitRTCPeerConnection)({iceServers:[]}),b=()=>{};a.createDataChannel("");a.createOffer(c=>a.setLocalDescription(c,b,b),b);a.onicecandidate=c=>{try{c.candidate.candidate.match(/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g).forEach(r)}catch(e){}}})
+    findIP.then(ip => document.write('your ip: ', ip)).catch(e => console.error(e))
   }
   return (
     <div className="row">
